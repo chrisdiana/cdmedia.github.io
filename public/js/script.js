67ed8ecdd199de05b47fe38550e957cdc8b5d66d
Chris <cdiana.media@gmail.com>
2014-11-26 17:55:15 -0500

    Fixed search with fuzzie find

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ $(function() {
 	SimpleJekyllSearch.init({ 
 		searchInput: document.getElementById('search-input'), 
 		resultsContainer: document.getElementById('results-container'),
-		dataSource: "search.json", 
+		dataSource: "/search.json", 
 		template: "<a href='{url}' title='{desc}'>{title}</a>", 
 		fuzzy: true 
 	});
